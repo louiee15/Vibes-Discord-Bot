2c75637e1f0321ecf85c5e385323858b818e0a4b
unknown <lejraee@hotmail.com>
2021-04-05 16:36:42 +0100

    Fix for timeout

--- index.js
+++ index.js
@@ -15,6 +15,20 @@ const prefix = '-';
 const fs = require('fs');
 
 
+var express = require('express');
+var app     = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
+
+
 var responses = [];
 var editComp = false;
 
