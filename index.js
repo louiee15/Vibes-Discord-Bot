615ea5ea4871ff169125d9067eeb704be4883281
unknown <lejraee@hotmail.com>
2021-04-12 20:45:33 +0100

    Participants List Fixed 2

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.listen(PORT, () => {
     wakeUpDyno(DYNO_URL); // will start once server starts
 })
 
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT || 5000));
 
 //For avoidong Heroku $PORT error
 app.get('/', function(request, response) {
