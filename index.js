bfaa931b6f0252f73e2a924bf3d054a9b097afe2
unknown <lejraee@Hotmail.com>
2021-05-27 16:11:40 +0100

    repl

--- index.js
+++ index.js
@@ -16,29 +16,16 @@ const fs = require('fs');
 
 
 const express = require('express');
-var app     = express();
+var app = express();
 
 const fetch = require("node-fetch");
 
-const wakeUpDyno = require("./wokeDyno.js");
-
 
 const PORT = 5000;
-const DYNO_URL = "https://vibesdiscord-bot.herokuapp.com/";
 
-app.listen(PORT, () => {
-    wakeUpDyno(DYNO_URL); // will start once server starts
-})
 
 app.set('port', (process.env.PORT || 5000));
 
-//For avoidong Heroku $PORT error
-app.get('/', function(request, response) {
-    var result = 'App is running'
-    response.send(result);
-}).listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get('port'));
-});
 
 
 var responses = [];
